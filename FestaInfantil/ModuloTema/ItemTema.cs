dcbcadad8a2bff1dd9abd8ee2283099647836102
jrmpac <junior.mpacheco@gmail.com>
2023-06-15 01:35:44 -0300

    Implementado objeto Item Tema.

---
+++ FestaInfantil/ModuloTema/ItemTema.cs
@@ -0,0 +1,12 @@
+﻿namespace FestaInfantil.ModuloTema
+{
+    public class ItemTema
+    {
+        public string Nome { get; set; }
+
+        public ItemTema(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}

7530a2e3722d10595c83b2dc89f1f2869a860555
davsw <daviwctt@gmail.com>
2023-06-16 15:41:58 -0300

    ...

--- FestaInfantil/ModuloFest/Festa.cs
+++ FestaInfantil/ModuloFest/Festa.cs
@@ -1,10 +1,5 @@
 ﻿using FestaInfantil.Compartilhado;
 using FestaInfantil.ModuloClientes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FestaInfantil.ModuloFest
 {

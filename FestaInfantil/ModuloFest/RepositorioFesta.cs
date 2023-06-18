612dc5c67961460f407fb376474885e51bc30862
jrmpac <junior.mpacheco@gmail.com>
2023-06-18 06:51:19 -0300

    Refatoração Repositorio Festa

--- FestaInfantil/ModuloFest/RepositorioFesta.cs
+++ FestaInfantil/ModuloFest/RepositorioFesta.cs
@@ -1,41 +1,12 @@
-﻿using FestaInfantil.ModuloClientes;
+﻿using FestaInfantil.Compartilhado;
+using FestaInfantil.ModuloClientes;
 namespace FestaInfantil.ModuloFest
 {
-    public class RepositorioFesta
+    public class RepositorioFesta : RepositorioBase<Festa>
     {
-        List<Festa> festas = new List<Festa>();
-        private static int contador;
-
-        public void Inserir(Festa festa)
+        public RepositorioFesta(List<Festa> festas)
         {
-            contador++;
-            festa.id = contador;
-            festas.Add(festa);
-        }
-        public void Editar(Festa festa)
-        {
-            Festa festaSelecionada = SelecionarPorId(festa.id);
-
-            festaSelecionada.endereco = festa.endereco;
-            festaSelecionada.dataCriacao = festa.dataCriacao;
-            festaSelecionada.horaInicial = festa.horaInicial;
-            festaSelecionada.horaFinal = festa.horaFinal;
-            festaSelecionada.cliente = festa.cliente;
-        }
-
-        public Festa SelecionarPorId(int id)
-        {
-            return festas.FirstOrDefault(x => x.id == id);
-        }
-
-        public List<Festa> GetFestas()
-        {
-            return festas;
-        }
-
-        public void Excluir(Festa festaSelecionada)
-        {
-           festas.Remove(festaSelecionada);
+            this.listaRegistros = festas;
         }
     }
 }

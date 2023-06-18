09e9941e98a00ad4d40df3cef13f67757adebe88
jrmpac <junior.mpacheco@gmail.com>
2023-06-18 12:14:36 -0300

    Implementado Repositorio Aluguel

---
+++ FestaInfantil/ModuloAluguel/RepositorioAluguel.cs
@@ -0,0 +1,12 @@
+﻿using FestaInfantil.Compartilhado;
+
+namespace FestaInfantil.ModuloAluguel
+{
+    public class RepositorioAluguel : RepositorioBase<Aluguel>
+    {
+        public RepositorioAluguel(List<Aluguel> alugueis)
+        {
+            this.listaRegistros = alugueis;
+        }
+    }
+}

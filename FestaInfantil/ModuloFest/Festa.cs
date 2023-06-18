02cded1f24e2cdd9dde6ebea9cde3527bd60fdee
jrmpac <junior.mpacheco@gmail.com>
2023-06-18 06:50:54 -0300

    Refatoração Entidade Festa

--- FestaInfantil/ModuloFest/Festa.cs
+++ FestaInfantil/ModuloFest/Festa.cs
@@ -1,29 +1,35 @@
 ﻿using FestaInfantil.Compartilhado;
 using FestaInfantil.ModuloClientes;
+using FestaInfantil.ModuloTema;
 
 namespace FestaInfantil.ModuloFest
 {
     public class Festa : EntidadeBase<Festa>
-    {
-        public int id;
+    {        
         public string endereco;
         public DateTime dataCriacao;
-        public string horaInicial;
-        public string horaFinal;
+        public TimeSpan horaInicial;
+        public TimeSpan horaFinal;
         public Cliente cliente;
+        public Tema tema;
 
-        public Festa(string endereco, DateTime dataCriacao, string horaInicial, string horaFinal, Cliente cliente)
+        public Festa(string endereco, DateTime dataCriacao, TimeSpan horaInicial, TimeSpan horaFinal, Cliente cliente, Tema tema)
         {
             this.endereco = endereco;
             this.dataCriacao = dataCriacao;
             this.horaInicial = horaInicial;
             this.horaFinal = horaFinal;
             this.cliente = cliente;
+            this.tema = tema;
         }
 
         public override void AtualizarInformacoes(Festa registroAtualizado)
         {
-            throw new NotImplementedException();
+            this.endereco = registroAtualizado.endereco;
+            this.horaInicial = registroAtualizado.horaInicial;
+            this.horaFinal = registroAtualizado.horaFinal;
+            this.cliente = registroAtualizado.cliente;
+            this.tema = registroAtualizado.tema;
         }
 
         public override string[] Validar()
@@ -33,12 +39,6 @@ namespace FestaInfantil.ModuloFest
 
             if (string.IsNullOrEmpty(endereco))
                 erros.Add("O campo 'endereco' é obrigatório");
-
-            else if (string.IsNullOrEmpty(horaInicial))
-                erros.Add("O campo 'hora inicial' é obrigatório");
-
-            else if (string.IsNullOrEmpty(horaFinal))
-                erros.Add("O campo 'hora final' é obrigatório");
 
             return erros.ToArray();
         }
